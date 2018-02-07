4286e34207c448fb88bda6b76921946c9bb820d9
Matt Smith <boulderfans@comcast.net>
2018-02-06 20:56:15 -0700

    SparkPrime

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -3,26 +3,6 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{SparkSession, DataFrame}
 
-// REGION TYPE,REGION,YEAR,WEEK,% WEIGHTED ILI,%UNWEIGHTED ILI,AGE 0-4,AGE 25-49,AGE 25-64,AGE 5-24,AGE 50-64,AGE 65,ILITOTAL,NUM. OF PROVIDERS,TOTAL PATIENTS
-/*
-case class ILIRecord(
-  regionType: String,
-  region: String,
-  year: Int,
-  week: Int,
-  percWeightedILI: Float,
-  unWeightedILI: Float,
-  age0To4: Long,
-  age25To49: Long,
-  age25To64: Long,
-  age5To24: Long,
-  age50To64: Long,
-  age65: Long,
-  ILITotal: Long,
-  numProviders: Int,
-  totalPatients: Int) {
-}*/
-
 object SimpleApp {
   def isPrime(testValue: Int): Boolean = {
     val previousValues = (2 until testValue).toList
@@ -46,7 +26,10 @@ object SimpleApp {
     //val dataFile = "d:/Projects/spark/testproj/ILINet.csv" // Should be some file on your system
     //val conf = new SparkConf().setAppName("Spark Test").setMaster("local[4]")
     val spark = SparkSession.builder.master("local").appName("Spark Test").getOrCreate()
-    val result = spark.sparkContext.parallelize(Seq(1,2,3,4,5), 4).map{ i => i*i }
+    val testValue = 5
+    val result = spark.sparkContext.parallelize(2 until testValue, 4).map{ i =>
+      if(testValue % i == 0) true else false
+    }.collect()
 //    val df: DataFrame = spark.read.format("csv").option("header", "true").load(dataFile)
 //    df.createOrReplaceTempView("cdc")
 //
